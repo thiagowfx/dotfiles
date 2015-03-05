af752129957d49fa31ff0adf89b5f0c691909d77
Thiago Perrotta <perrotta.thiago@poli.ufrj.br>
2015-03-04 22:57:55 -0300

    addenv, addalias, addenv tests
    Former-commit-id: d6ff1ec5fb88849b19a47584931f71c819a6a21f
    Former-commit-id: cf2cfe99c0c438587fa1a09915bd82a35df2e51a

--- tests.sh
+++ tests.sh
@@ -6,3 +6,17 @@
 assert "echo 42" "42"
 assert_raises "exit 10" "10"
 assert_end assert.sh
+
+#
+# .alias tests
+#
+
+# addenv tests
+source .alias
+assert 'addenv DUMMYVAR "42"; echo $DUMMYVAR' '42'
+assert 'addenv DUMMYVAR ""; echo $DUMMYVAR' ''
+assert 'addenv EDITOR "vi" vi; command -v &>/dev/null vi && echo $EDITOR' 'vi'
+assert 'addenv EDITOR "vifake_" vifakefake; command -v vifake_ && echo $EDITOR' ''
+assert 'addenv MYPATH "dummypath" "" "/qwqw_qwqw_az"; echo $MYPATH' ''
+assert 'mkdir /tmp/qwqw_; addenv MYPATH "dummypath" "" "/tmp/qwqw_"; echo $MYPATH' 'dummypath'
+assert_end aliases
