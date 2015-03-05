8d1a7ed38465fa36850015a954c3671e0b324034
Thiago Perrotta <perrotta.thiago@poli.ufrj.br>
2015-03-05 00:08:43 -0300

    .alias
    Former-commit-id: b8c52c88f60a360fec5a19ece8f3c75dc0579fdc
    Former-commit-id: 46ba8f41aaf5d49b6dbae645bd92d29286ded582

--- tests.sh
+++ tests.sh
@@ -10,13 +10,20 @@ assert_end assert.sh
 #
 # .alias tests
 #
+source .alias
 
 # addenv tests
-source .alias
 assert 'addenv DUMMYVAR "42"; echo $DUMMYVAR' '42'
 assert 'addenv DUMMYVAR ""; echo $DUMMYVAR' ''
 assert 'addenv EDITOR "vi" vi; command -v &>/dev/null vi && echo $EDITOR' 'vi'
 assert 'addenv EDITOR "vifake_" vifakefake; command -v vifake_ && echo $EDITOR' ''
 assert 'addenv MYPATH "dummypath" "" "/qwqw_qwqw_az"; echo $MYPATH' ''
 assert 'mkdir /tmp/qwqw_; addenv MYPATH "dummypath" "" "/tmp/qwqw_"; echo $MYPATH' 'dummypath'
-assert_end aliases
+assert 'mkdir /tmp/qwqw_; addenv MYPATH "dummypath" fake_ "/tmp/qwqw_"; echo $MYPATH' ''
+assert 'mkdir /tmp/qwqw_; addenv MYPATH "dummypath" vi "/tmp/qwqw_"; echo $MYPATH' 'dummypath'
+assert_end addenv
+
+# addalias tests
+# it is not possible to do more tests with assert.sh
+assert 'addalias hello "echo world" fake_; hello' ''
+assert_end addalias
