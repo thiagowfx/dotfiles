b090797afad70728f5489ed9f7c9536c1458ca8e
Thiago Perrotta <perrotta.thiago@poli.ufrj.br>
2015-03-26 14:05:11 -0300

    finish scaffold.sh
    Former-commit-id: 2acd233d21e149622de8da9973ca339c0bf15e83
    Former-commit-id: 4619d21f94066a5b15d0f9cc5cf8e7fc2d071df7

--- idesktop/scaffold.sh
+++ idesktop/scaffold.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 
+usage() {
+    cat <<HERE
+"\$1" -- executable
+"\$2" -- caption
+"\$3" -- tooltip
+"\$4" -- path to icon
+HERE
+}
+
+if [ "$1" == "" ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]
+then
+    usage
+    exit 0
+fi
+
+cat >"$1.lnk" <<HERE
 table Icon
-  Caption: "$2"
-  ToolTip.Caption: "$3"
+  Caption: "${2:-$1}"
+  ToolTip.Caption: "${3:-$1}"
   Icon: "$4"
   Width: 32
   Height: 32
@@ -10,4 +26,5 @@ table Icon
   Y: 500
   Command[0]: "$1"
 end
-
+HERE
+echo "--> created "$1".lnk"
